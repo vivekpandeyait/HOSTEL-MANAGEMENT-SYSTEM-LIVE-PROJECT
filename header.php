52b376ae7dbd5c9327af0ce287465124ca764b85
vivekpandeyait <vivek496pandey@gmail.com>
2021-02-10 19:18:30 +0530

    HOSTEL MANAGEMENT SYSTEM  LIVE PROJECT IN PHP & MYSQL

---
+++ header.php
@@ -0,0 +1,81 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=2, initial-scale=1.0">
+    <title>Hotelspaze</title>
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="icon" href="img/favicon1.png" type="image/gif">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <style>
+              
+              .float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+  font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+
+.my-float{
+	margin-top:16px;
+}
+          </style>
+</head>
+ 
+ <nav class="navbar navbar-expand-md  fixed-top stroke">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <a href="index1.php" class="navbar-brand"><img src="img/Hostelspaze2.png" height="28px" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+          </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+
+                    
+                    <li>
+                        <select class="form-control tm-select" >
+                          <option value="0 ">Location</option>
+                          <option value="1 ">London</option>
+                          <option value="2 ">Singapore</option>
+                          <option value="3 ">Malaysia</option>
+                         <option value="4 ">Austria</option>
+                            <option value="3 ">Scotland</option>
+                        </select>
+                    </li>
+                    <li>
+                        <a href="service.php">
+                            <i class="fa fa-fw fa-handshake-o " style="color:#1b1a25ff; "></i>Services
+                        </a>
+                    </li>
+                    <li>
+                        <a href="ourteam.php">
+                            <i class="fa fa-fw fa-users " style="color:#1b1a25ff; "></i> Team
+                        </a>
+                    </li>
+
+
+                    
+                    <li>
+                        <a href="contact.php">
+                            <i class="fa fa-fw fa-phone " style="color:#1b1a25ff; "></i> Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#login ">
+                            <i class="fa fa-fw fa-user " style="color:#1b1a25ff; "></i>login
+                        </a>
+            </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ test.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+</head>
+
+<body>
+     <div  >
+    <?php include 'header.php'; ?>
+   
+       
+        <br><br>        <br><br>
+        <hr>
+        <center><h3 style="color:black">Our Team testing</h3></center>
+        <hr>
+        <div id="main ">
+        <div class="inner ">
+            <div class="thumbnails ">
+
+                <div class="box ">
+                    <a href="# " class="image fit "><center><img src="img_avatar/img_avatar.png" alt=" " style="width:200px ;border-radius: 50%; " /></center></a>
+                    <div class="inner ">
+                        <h3>Bhanu Prakash</h3>
+                        <p>Founder and CEO</p>
+                        <p><i class="icon-phone"></i> +919620713899</p>
+                      <p>  <i class="icon-envelope-alt"></i> bhanu@hostelspaze.com</p>
+                        
+                    </div>
+                </div>
+
+                <div class="box ">
+                    <a href="# " class="image fit "><center><img src="img_avatar/img_avatar1.png" alt=" " style="width:200px ;border-radius: 50%;" /></center></a>
+                    <div class="inner ">
+                        <h3>Vartika Tiwari</h3>
+                        <p>Co-founder</p>
+                       
+                    </div>
+                </div>
+
+                <div class="box ">
+                    <a href="# " class="image fit "><center><img src="img_avatar/img_avatar.png" alt=" " style="width:200px ;border-radius: 50%;" /></center></a>
+                    <div class="inner ">
+                        <h3>Vivek Pandey</h3>
+                        <p>Developer</p>
+                       
+                        
+                    </div>
+                </div>
+                 <div class="box ">
+                    <a href="# " class="image fit "><center><img src="img_avatar/img_avatar.png" alt=" " style="width:200px ;border-radius: 50%;" /></center></a>
+                    <div class="inner ">
+                        <h3>Amar Raj</h3>
+                        <p>Developer</p>
+                        
+                    </div>
+                </div>
+
+               
+                
+
+               
+
+                
+
+            </div>
+
+        </div>
+    </div>
+        
+        
+        
+        
+       
+    
+
+ <?php include 'footer.php'; ?>
+  </div>
+  
+     <script src="js/bootstrap.min.js "></script>
+    <script src="js/footer.js "></script>
+    <script src="js/jquery-min.js "></script>
+ 
+</body>
+
+</html>
